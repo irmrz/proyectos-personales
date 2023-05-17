abee1ec06bbf4bb6c823fda9e8606dc9616bc1b5
irmrz <ignaciotramirez02@gmail.com>
2023-05-17 00:19:33 -0300

    falta acomodar la linea después del }

--- python/formatter/my-formatter.py
+++ python/formatter/my-formatter.py
@@ -1,3 +1,4 @@
+
 def readFile(input):
     try:
         with open(input, 'rb') as f:
@@ -6,17 +7,74 @@ def readFile(input):
         print("Error: File does not appear to exist.")
     return byte_string
 
-def newFile(byteString):
-    newArray = []
-    for byte in byteString:
+def bracket_counter(byte_string):
+    stack = []
+    positions = []
+    brackets = []
+    counter = 0
+    i = 0
+    for byte in byte_string:
         if byte == 123:
-            newArray.append(10)
-            newArray.append(byte)
-        else:
-            newArray.append(byte)
-    result = ''.join(chr(i) for i in newArray)
-    return result.encode("ascii")
+            stack.append(counter)
+            counter += 1
+            positions.append(i)
+            brackets.append(byte)
+        elif byte == 125:
+            counter -= 1
+            stack.append(counter)
+            positions.append(i)
+            brackets.append(byte)
+        i = i + 1
+    return stack, positions, brackets
 
-print(readFile('test.c'))
-print("\nAfter formatting: ")
-print(newFile(readFile('test.c')))
+
+def newString(byte_string, stack: list, positions: list, brackets: list):
+    new_string = []
+    for i in range(len(byte_string)):
+        if i == positions[0]:
+            if brackets[0] == 123:
+                new_string.append(10)
+                j = 0
+                while j < stack[0]*4 :
+                    new_string.append(32)
+                    j += 1
+
+                new_string.append(byte_string[i])
+                new_string.append(10)
+
+                positions.pop(0)
+                brackets.pop(0)
+                stack.pop(0)
+            else:
+                
+                while j < stack[0]*4 :
+                    new_string.append(32)
+                    j += 1
+                new_string.append(byte_string[i])
+                new_string.append(10)
+
+                positions.pop(0)
+                brackets.pop(0)
+                stack.pop(0)
+                
+        else:
+            new_string.append(byte_string[i])
+
+    return new_string
+
+def listToString(s):
+    str1 = ""
+    for ele in s:
+        str1 += chr(ele)
+    return str1
+
+def main():
+    byte_string = readFile('test.c')
+    stack, positions, brackets = bracket_counter(byte_string)
+    new_string = newString(byte_string, stack, positions, brackets)
+    s1 = listToString(new_string)
+
+    print(s1)
+
+if __name__ == "__main__":
+    main()
--- python/formatter/test.c
+++ python/formatter/test.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
 void main() {
     print("Hello World!");
+    int x = 0;
+    if (x == 0) {
+        printf("x is 0");
+    }else {
+        printf("x is not 0");
+    }
 }
