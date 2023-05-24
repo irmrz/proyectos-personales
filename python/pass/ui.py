704455b6f597dea9d36ed196bd42aa8f963f7109
irmrz <ignaciotramirez02@gmail.com>
2023-05-24 20:25:38 -0300

    makefile añadido, opcion para compilar ejecutable

--- python/pass/ui.py
+++ python/pass/ui.py
@@ -13,13 +13,14 @@ def main():
     columns = Columns([text], width=120, align='left')
     console.print(columns)
 
-    console.print("How many characters do you want your password to have? (must be greater than 4 and smaller than 129 (not inclusive))", style="b sky_blue2")
+    console.print("How many characters do you want in your password?", style="b sky_blue2")
+    console.print("The minimum is 5 characters and the maximum is 128.", style="b sky_blue2")
     user_input = read_user_input()
     result = password(user_input)
     console.print("Your password is:", style="b sky_blue2")
     console.print(Text(result), style="bold ")
     console.print("Your password has been copied to your clipboard!", style="b sky_blue2")
-    pyperclip.copy(password(user_input))
+    pyperclip.copy(result)
     console.print("Copyright@ Ignacio Ramirez", style="grey15")
 
 if __name__ == '__main__':
